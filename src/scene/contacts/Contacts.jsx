11d6b9585bd29ba43162a097bb787cd0ec92bd9a
Hossam Ragab <hossam.alasklany@gmail.com>
2023-01-19 17:13:21 +0200

    contact Done

--- src/scene/contacts/Contacts.jsx
+++ src/scene/contacts/Contacts.jsx
@@ -6,7 +6,7 @@ import { mockDataContacts } from '../../data/mockData';
 import Header from '../../components/Header';
 
 
-const Team = () => {
+const Contacts = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
@@ -101,4 +101,4 @@ const Team = () => {
     );
 };
 
-export default Team;
+export default Contacts;
