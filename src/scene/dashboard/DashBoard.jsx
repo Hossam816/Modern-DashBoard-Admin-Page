407c14593d532548a209125f9b7791360e933e17
Hossam Ragab <hossam.alasklany@gmail.com>
2023-01-19 14:50:43 +0200

    a

--- src/scene/dashboard/DashBoard.jsx
+++ src/scene/dashboard/DashBoard.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
+import Header from "../../components/Header";
+import { Box } from "@mui/material";
 
 const DashBoard = () => {
     return (
-        <div>Dashboard</div>
+        <Box m="20px">
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Header title="DASHBOARD" subTitle="Welcome to your dashboard" />
+            </Box>
+        </Box>
     );
 };
 
