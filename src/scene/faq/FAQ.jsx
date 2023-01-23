614cff4d6e9d030ba4b492112d6c48adbd770c49
Hossam Ragab <hossam.alasklany@gmail.com>
2023-01-23 19:18:57 +0200

    Question Components Done

--- src/scene/faq/FAQ.jsx
+++ src/scene/faq/FAQ.jsx
@@ -1,8 +1,69 @@
 import React from 'react';
+import { Box, useTheme, Typography } from '@mui/material';
+import Header from "../../components/Header";
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { tokens } from '../../theme';
 
 const FAQ = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
     return (
-        <div>FAQ</div>
+        <Box m="20px">
+            <Header title="FAQ" subTitle="Frequently Asked Questions Page" />
+
+            <Accordion defaultExpanded>
+                <AccordionSummary e expandIcon={ <ExpandMoreIcon /> } >
+                    <Typography color={ colors.greenAccent[ 500 ] } variant='h5'>
+                        An Important Question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam necessitatibus qui, quos veritatis reiciendis aliquid molestiae et autem nobis odit.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+                <AccordionSummary e expandIcon={ <ExpandMoreIcon /> } >
+                    <Typography color={ colors.greenAccent[ 500 ] } variant='h5'>
+                        Your Fav Question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam necessitatibus qui, quos veritatis reiciendis aliquid molestiae et autem nobis odit.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+                <AccordionSummary e expandIcon={ <ExpandMoreIcon /> } >
+                    <Typography color={ colors.greenAccent[ 500 ] } variant='h5'>
+                        Some Random Question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam necessitatibus qui, quos veritatis reiciendis aliquid molestiae et autem nobis odit.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={ <ExpandMoreIcon /> } >
+                    <Typography color={ colors.greenAccent[ 500 ] } variant='h5'>
+                        The Final Question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam necessitatibus qui, quos veritatis reiciendis aliquid molestiae et autem nobis odit.
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+        </Box>
     );
 };
 
