358bfc6deff11c8a13b1380f78387b0e8f2a289d
Hossam Ragab <hossam.alasklany@gmail.com>
2023-01-25 15:30:13 +0200

    modification

--- src/scene/geography/Geography.jsx
+++ src/scene/geography/Geography.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Box, colors } from '@mui/material';
+import Header from "../../components/Header";
+import GEoChart from '../../components/GEoChart';
 
 const Geography = () => {
     return (
-        <div>Geography</div>
+        <Box m="20px">
+            <Header title="GEO Chart" subTitle="Simple GEO Chart" />
+            <Box height="75vh" border={ `1.5px solid ${colors.grey[ 600 ]}` } borderRadius="4px">
+                <GEoChart />
+            </Box>
+        </Box>
     );
 };
 
