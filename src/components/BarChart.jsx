aebbd9063d921d42102032332ceede45053a8f84
Hossam Ragab <hossam.alasklany@gmail.com>
2023-01-25 14:35:39 +0200

    styleing done in the charts

---
+++ src/components/BarChart.jsx
@@ -0,0 +1,163 @@
+import React from 'react';
+import { ResponsiveBar } from '@nivo/bar';
+import { useTheme } from '@mui/material';
+import { tokens } from '../theme';
+import { mockBarData as data } from '../data/mockData';
+
+const BarChart = ({ isDashboard = false }) => {
+    const them = useTheme();
+    const colors = tokens(them.palette.mode);
+
+    return (
+        <ResponsiveBar
+            data={ data }
+            them={ {
+                axis: {
+                    domain: {
+                        line: {
+                            stroke: colors.gray[ 100 ]
+                        },
+                    },
+                    legend: {
+                        text: {
+                            fill: colors.gray[ 100 ]
+                        },
+                    },
+                    ticks: {
+                        line: {
+                            stroke: colors.gray[ 100 ],
+                            strokeWidth: 1,
+                        },
+                        text: {
+                            fill: colors.gray[ 100 ]
+                        },
+                    },
+                },
+                legends: {
+                    text: {
+                        fill: colors.gray[ 100 ]
+                    }
+                }
+            } }
+            keys={ [
+                'hot dog',
+                'burger',
+                'sandwich',
+                'kebab',
+                'fries',
+                'donut'
+            ] }
+            indexBy="country"
+            margin={ { top: 50, right: 130, bottom: 50, left: 60 } }
+            padding={ 0.45 }
+            innerPadding={ 2 }
+            valueScale={ { type: 'linear' } }
+            indexScale={ { type: 'band', round: true } }
+            valueFormat=" >+,"
+            colors={ { scheme: 'pastel2' } }
+            defs={ [
+                {
+                    id: 'dots',
+                    type: 'patternDots',
+                    background: 'inherit',
+                    color: '#38bcb2',
+                    size: 4,
+                    padding: 1,
+                    stagger: true
+                },
+                {
+                    id: 'lines',
+                    type: 'patternLines',
+                    background: 'inherit',
+                    color: '#eed312',
+                    rotation: -45,
+                    lineWidth: 6,
+                    spacing: 10
+                }
+            ] }
+            fill={ [
+                {
+                    match: {
+                        id: 'fries'
+                    },
+                    id: 'dots'
+                },
+                {
+                    match: {
+                        id: 'sandwich'
+                    },
+                    id: 'lines'
+                }
+            ] }
+            borderRadius={ 5 }
+            borderColor={ {
+                from: 'color',
+                modifiers: [
+                    [
+                        'darker',
+                        '1.3'
+                    ]
+                ]
+            } }
+            axisTop={ null }
+            axisRight={ null }
+            axisBottom={ {
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: isDashboard ? undefined : 'country',
+                legendPosition: 'middle',
+                legendOffset: 32
+            } }
+            axisLeft={ {
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: isDashboard ? undefined : 'food',
+                legendPosition: 'middle',
+                legendOffset: -40
+            } }
+            enableGridX={ true }
+            labelSkipWidth={ 20 }
+            labelSkipHeight={ 7 }
+            labelTextColor={ {
+                from: 'color',
+                modifiers: [
+                    [
+                        'darker',
+                        '3'
+                    ]
+                ]
+            } }
+            legends={ [
+                {
+                    dataFrom: 'keys',
+                    anchor: 'bottom-right',
+                    direction: 'column',
+                    justify: false,
+                    translateX: 128,
+                    translateY: 0,
+                    itemsSpacing: 6,
+                    itemWidth: 112,
+                    itemHeight: 26,
+                    itemDirection: 'left-to-right',
+                    itemOpacity: 0.85,
+                    symbolSize: 18,
+                    effects: [
+                        {
+                            on: 'hover',
+                            style: {
+                                itemOpacity: 1
+                            }
+                        }
+                    ]
+                }
+            ] }
+            motionConfig="gentle"
+            role="application"
+            ariaLabel="Nivo bar chart demo"
+            barAriaLabel={ function (e) { return e.id + ": " + e.formattedValue + " in country: " + e.indexValue; } }
+        />
+    );
+};
+export default BarChart;
